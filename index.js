c25e4ca84f5a5a3ddd284abcc2ff4cc9774e900e
nabilhayet <nabil4457@yahoo.com>
2020-10-24 21:33:45 -0400

    Done.

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, alert) {
+      if (Array.isArray(collection) === true) {
+        for(let i=0;i<collection.length;i++){
+          alert(`This number is ${collection[i]}`)
+          }
+      }
+      else {
+        
+      }
+      return collection
+    },
+
+    map: function(collection,callback) {
+
 
     },
 
-    map: function() {
-
-    },
-
-    reduce: function() {
+    reduce: function(collection, iterator, accumulator) {
+     
 
     },
 
